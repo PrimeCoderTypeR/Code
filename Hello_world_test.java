8f03b98e8f774e32b640b37349550cd2e1d909b6
PrimeCoderTypeR <2brickmaster@gmail.com>
2023-10-23 22:11:53 -0400

    first commit

---
+++ Hello_world_test.java
@@ -0,0 +1,6 @@
+public class Hello_world_test{
+    public static void main(String args[])
+    {
+        System.out.println("First Out input");
+    }
+}
---
+++ getinput.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class getinput {
+    public static void main(String args[]){
+
+        Scanner ask = new Scanner(System.in);
+        System.out.println(ask.nextLine());
+        ask.close();
+
+    }
+}
